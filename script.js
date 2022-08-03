f6351535be19f430f4c7916f4e5caa9d3a78d59d
tidynoob <mitchellgriffin9@gmail.com>
2022-08-03 14:55:19 -0400

    Add styling e.g. buttons, display

---
+++ script.js
@@ -0,0 +1,14 @@
+// CALCULATOR FUNCTIONS
+
+let add = (x,y) => {return x + y};
+
+let subtract = (x,y) => {return x - y};
+
+let multiply = (x,y) => {return x * y};
+
+let divide = (x,y) => {return x / y};
+
+let operate = (x,y,foo) => {
+    return foo(x,y)
+}
+
