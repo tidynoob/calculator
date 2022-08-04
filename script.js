93ba2d1d4f035a6cf8465508a02d988edd218124
tidynoob <mitchellgriffin9@gmail.com>
2022-08-03 23:12:40 -0400

    Adjust styling and add some button code
    I only added button code for numbers plus period.

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
 // CALCULATOR FUNCTIONS
 
-let add = (x,y) => {return x + y};
+let add = (x, y) => { return x + y };
 
-let subtract = (x,y) => {return x - y};
+let subtract = (x, y) => { return x - y };
 
-let multiply = (x,y) => {return x * y};
+let multiply = (x, y) => { return x * y };
 
-let divide = (x,y) => {return x / y};
+let divide = (x, y) => { return x / y };
 
-let operate = (x,y,foo) => {
-    return foo(x,y)
+let operate = (x, y, foo) => {
+    return foo(x, y)
 }
 
+// CLICK FUNCTIONS
+
+let adjustDisplay = (e) => {
+
+    let displayText = document.querySelector('.displayText');
+    let text = (displayText.textContent.replaceAll(',', '') || "");
+    // console.log(text);
+
+
+    let buttonText = (e.target.textContent || e.target.innerText);
+    // console.log(buttonText);
+
+    if (parseInt(buttonText)) {
+        text += String(buttonText);
+        displayText.textContent = Number(text).toLocaleString('en-US');
+
+    } else if (buttonText == '.') {
+        if (text.slice(-1) == '.') return;
+        text += String(buttonText);
+        displayText.textContent = text;
+    }
+}
+
+let buttons = document.querySelectorAll('button');
+// console.log(buttons);
+
+buttons.forEach(button => button.addEventListener('click', adjustDisplay));
