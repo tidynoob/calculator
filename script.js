a109d112b5004adf9d5810eb598291d42fc113a2
tidynoob <mitchellgriffin9@gmail.com>
2022-08-04 08:23:45 -0400

    Adjust button functionality in changing display

--- script.js
+++ script.js
@@ -14,25 +14,57 @@ let operate = (x, y, foo) => {
 
 // CLICK FUNCTIONS
 
+let adjustText = (text, buttonText, displayText) => {
+    if (parseInt(buttonText)) {
+        text += String(buttonText);
+        displayText.textContent = Number(text).toLocaleString('en-US');
+
+    } else {
+        switch (buttonText) {
+            // case '.':
+            //     if (text.slice(-1) == '.') return;
+            //     text += String(buttonText);
+            //     displayText.textContent = text;
+            //     break;
+
+            case '+/-':
+                if (text == '') return;
+                if (text.charAt(0) == '-') {
+                    text = text.substring(1);
+                    displayText.textContent = Number(text).toLocaleString('en-US');
+                } else {
+                    text = '-' + text;
+                    displayText.textContent = Number(text).toLocaleString('en-US');
+                }
+                break;
+
+            case 'AC':
+                displayText.textContent = '';
+                break;
+
+            case 'CE':
+                displayText.textContent = '';
+                break;        
+                
+            default:
+                if (!parseInt(text.slice(-1))) return;
+                text = displayText.textContent + String(buttonText);
+                displayText.textContent = text;
+        }
+    }
+
+}
+
 let adjustDisplay = (e) => {
 
     let displayText = document.querySelector('.displayText');
     let text = (displayText.textContent.replaceAll(',', '') || "");
     // console.log(text);
 
-
     let buttonText = (e.target.textContent || e.target.innerText);
     // console.log(buttonText);
 
-    if (parseInt(buttonText)) {
-        text += String(buttonText);
-        displayText.textContent = Number(text).toLocaleString('en-US');
-
-    } else if (buttonText == '.') {
-        if (text.slice(-1) == '.') return;
-        text += String(buttonText);
-        displayText.textContent = text;
-    }
+    adjustText(text, buttonText, displayText);
 }
 
 let buttons = document.querySelectorAll('button');
